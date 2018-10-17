2238a03520c2c3379310b0e8b8729916f8fa5e96
Roland4444 <turbopollux@rambler.ru>
2018-10-17 16:17:23 +0000

    need to test serrvice

--- src/main/scala/EBS.scala
+++ src/main/scala/EBS.scala
@@ -1,7 +1,8 @@
-import java.io.{File, PrintWriter}
+import java.io.{BufferedOutputStream, File, FileOutputStream, PrintWriter}
 import java.nio.file.{Files, Path, Paths}
 
 import Essentials.{Photo, Voice}
+import RawImplements.callEBS_sound
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
@@ -21,8 +22,15 @@ object forTest{
   val nothing = "nothing"
 }
 
-class GetType extends Actor {
+object EBS_replyes{
+  val PASSED = "OK"
+  val FAILED = "failed"
 
+
+}
+
+class GetType extends Actor {
+  val cebs = new callEBS_sound
   def writeResult(): Unit ={
     val pr = new PrintWriter("outs");
     pr.write(returned);
@@ -31,16 +39,35 @@ class GetType extends Actor {
   var returned: String=""
   def receive = {
     case f: Voice      => {
+      val bos = new BufferedOutputStream(new FileOutputStream(f.filename))
+      bos write(f.wavContent)
+      bos close
+      val rc = cebs.call_ebs("./cv_configuration.json", s"./${f.filename}")
+      if (rc.checkResult==0)
+        sender ! EBS_replyes.PASSED
+      else
+        sender ! EBS_replyes.FAILED
+      val filename = new File(f.filename)
+      filename.delete()
+
       println("==>Just received  Voice")
       returned = forTest.voice
       writeResult
     }
     case f: Photo      => {
+      val bos = new BufferedOutputStream(new FileOutputStream(f.filename))
+      bos write(f.wavContent)
+      bos close
+
+
       println("==>Just received  Photo")
       returned = forTest.photo
       writeResult
     }
     case _ => {
+
+
+
       println("nothing")
       returned = forTest.nothing
       writeResult
